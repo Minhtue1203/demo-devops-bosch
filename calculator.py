b8d4ce1eb13adb47cade680651f67afdef9ee12b
Minh-Tue Tran <minhtue1203@gmail.com>
2026-05-26 00:38:31 +0200

    feat: add factorial function without test :)

--- calculator.py
+++ calculator.py
@@ -6,3 +6,17 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
+
+def factorial(n):
+    if n < 0:
+        raise ValueError("Negative number not allowed")
+
+    if n == 0:
+        return 1
+
+    result = 1
+
+    for i in range(1, n + 1):
+        result *= i
+
+    return result
